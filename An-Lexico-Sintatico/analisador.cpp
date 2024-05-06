a60f2ec9473568f277fe535227759201bc32581b
Vina77 <marcus300702@outlook.com>
2024-05-06 15:57:01 -0300

    Analisador Léxico, pt3

--- An-Lexico-Sintatico/analisador.cpp
+++ An-Lexico-Sintatico/analisador.cpp
@@ -4,11 +4,12 @@
 
 int main()
 {
-    //std::string userInput = "2+3-x=1";
     std::string userInput;
+    int operandCount = 0;
+    int operatorCount = 0;
 
     std::cout << "Coloque uma expressao matematica para ser analisada: " << std::endl;
-    std::cin >> userInput;
+    std::getline(std::cin, userInput);
 
     std::cout << "-----------------------------------" << std::endl;
     std::cout << "  Lexema  |  Token (tipos de token)" << std::endl;
@@ -19,11 +20,13 @@ int main()
         {
             std::cout << "     " << userInput[i] << "    |  "
                       << "Numero" << std::endl;
+            operandCount++;
         }
         else if (isalpha(userInput[i]))
         {
             std::cout << "     " << userInput[i] << "    |  "
                       << "Variavel" << std::endl;
+            operandCount++;
         }
         else if (userInput[i] == '(' || userInput[i] == ')' || userInput[i] == '{' || userInput[i] == '}' || userInput[i] == '[' || userInput[i] == ']' || userInput[i] == ',' || userInput[i] == '.' || userInput[i] == '\'' || userInput[i] == '\"')
         {
@@ -34,6 +37,7 @@ int main()
         {
             std::cout << "     " << userInput[i] << "    |  "
                       << "Operador" << std::endl;
+            operatorCount++;
         }
         else
         {
@@ -42,6 +46,11 @@ int main()
         }
     }
     std::cout << "-----------------------------------" << std::endl;
+
+    if (operandCount > 3 || operatorCount > 2)
+    {
+        std::cout << "A expressao inserida nao e valida. Ela deve conter tres operandos e no maximo dois operadores alem do de atribuicao." << std::endl;
+    }
 
     return 0;
 }
